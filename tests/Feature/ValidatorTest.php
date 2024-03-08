864219095f30bb21d7768397e06dc0dfe9dc4bbb
Whydandrian <whydandrian@gmail.com>
2024-03-08 23:13:16 +0700

    Custom rules validation to ID & add setlocal ID to ValidatorTest

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -92,6 +93,33 @@ class ValidatorTest extends TestCase
 
    public function testValidatorValidData()
    {
+      $data = [
+         'username' => 'wah',
+         'password' => 'rahasia',
+         'admin' => true,
+         'others' => false
+      ];
+      $rules = [
+         'username' => 'required|email|max:100',
+         'password' => 'required|min:6|max:20'
+      ];
+
+      $validator = Validator::make($data, $rules);
+      self::assertNotNull($validator);
+
+      try {
+         $valid = $validator->validate();
+         Log::info(json_encode($valid, JSON_PRETTY_PRINT));
+      } catch (ValidationException $exception) {
+         self::assertNotNull($exception->validator);
+         $message = $exception->validator->errors();
+         Log::error($message->toJson(JSON_PRETTY_PRINT));
+      }
+   }
+
+   public function testValidatorSetLocalization()
+   {
+      App::setLocale('id');
       $data = [
          'username' => 'wah',
          'password' => 'rahasia',
