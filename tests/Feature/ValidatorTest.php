3fc3b2d8f40cd0314b42350ca125b3ee0380da47
Whydandrian <whydandrian@gmail.com>
2024-03-08 12:48:13 +0700

    Test Validator Failed: Add LOG Info MsgBox Done

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -27,7 +28,7 @@ class ValidatorTest extends TestCase
       self::assertTrue($validator->passes());
       self::assertFalse($validator->fails());
    }
-   
+
    public function testValidatorInvalid()
    {
       $data = [
@@ -43,5 +44,10 @@ class ValidatorTest extends TestCase
       self::assertNotNull($validator);
       self::assertFalse($validator->passes());
       self::assertTrue($validator->fails());
+
+
+      $message = $validator->getMessageBag();
+
+      Log::info($message->toJson(JSON_PRETTY_PRINT));
    }
 }
