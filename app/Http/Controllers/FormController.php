6a75080050c1d3e52d27ac7cd59106265d10e76e
Whydandrian <whydandrian@gmail.com>
2024-03-09 12:11:49 +0700

    FormController: Add login function

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class FormController extends Controller
 {
-    //
+   public function login(Request $request): Response
+   {
+      try {
+         $rules = [
+            'username' => 'wahyudi',
+            'password' => 'rahasia'
+         ];
+         $data = request()->validate($rules);
+         // $data manipulation
+         return response('OK', Response::HTTP_OK);
+      } catch (ValidationException $exception) {
+         return response($exception->errors(), Response::HTTP_BAD_REQUEST);
+      }
+   }
 }
