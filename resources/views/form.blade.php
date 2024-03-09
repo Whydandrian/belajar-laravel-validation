884c48bb7cebf4909ffae5aeecca66759439ea9b
Whydandrian <whydandrian@gmail.com>
2024-03-09 14:42:58 +0700

    Form View: add repopulating value

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -21,11 +21,11 @@
     @csrf
     <label> 
       Username : @error('username') {{$message}} @enderror
-      <input type="text" name="username">
+      <input type="text" name="username" value="{{old('username')}}">
     </label><br>
     <label> 
       Password : @error('password') {{$message}} @enderror
-      <input type="password" name="password">
+      <input type="password" name="password" value="{{old('password')}}">
     </label><br>
     <input type="submit" value="Login">
   </form>
